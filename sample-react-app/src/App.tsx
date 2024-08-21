01c9f9e318f7650094ae1f105c323dc4152dd988
Ippei523 <65227036+Ippei523@users.noreply.github.com>
2024-08-21 15:28:55 +0900

    デフォルトを追加

--- sample-react-app/src/App.tsx
+++ sample-react-app/src/App.tsx
@@ -2,21 +2,29 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AxiosContainer } from "./component/axiosContainer";
 import { SwrContainer } from "./component/swrContainer";
-import { TanstackQueryContainer } from "./component/reactQueryContainer";
+import { TanstackQueryContainer } from "./component/tanstackQueryContainer";
 import { Home } from "./component/home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/axios" element={<AxiosContainer />} />
-          <Route path="/swr" element={<SwrContainer />} />
-          <Route path="/tanstack-query" element={<TanstackQueryContainer />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/axios" element={<AxiosContainer />} />
+            <Route path="/swr" element={<SwrContainer />} />
+            <Route
+              path="/tanstack-query"
+              element={<TanstackQueryContainer />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </QueryClientProvider>
   );
 }
 
